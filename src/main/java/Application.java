08c790a519c96272d2696acd5dc902cdb85e78dc
csbsjy <seojaeyeon@lotte.net>
2020-07-17 02:58:35 +0900

    feature : OrderService 구현 - Main 코드 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,8 @@
-import domain.Menu;
 import domain.MenuRepository;
+import domain.OrderRepository;
 import domain.Table;
 import domain.TableRepository;
+import service.CafeService;
 import view.InputView;
 import view.OutputView;
 
@@ -11,14 +12,22 @@ public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
         final List<Table> tables = TableRepository.tables();
-        final List<Menu> menus = MenuRepository.menus();
+        final CafeService cafeService = new CafeService(new OrderRepository());
 
         int selectNo = InputView.showMain();
 
         while (selectNo != 3) {
-            if (selectNo == 1) {
+            if (selectNo == 1) { // 주문
                 OutputView.printTables(tables);
                 int tableNumber = InputView.inputTableNumber();
+                OutputView.printMenus(MenuRepository.menus());
+                int menuNumber = InputView.inputMenuNumber();
+                cafeService.orderMenu(menuNumber, tableNumber);
+            }
+            if (selectNo == 2) {
+                OutputView.printTables(tables);
+                int tableNumber = InputView.inputTableNumber();
+                System.out.println("최종결제금액: " + cafeService.calculate(tableNumber));
             }
             selectNo = InputView.showMain();
         }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,4 +14,10 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 메뉴선택");
+        return scanner.nextInt();
+    }
+
 }
